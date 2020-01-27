bc097741340c66293c32eafaa2bebe4bd628c1f9
kyoro72901 <arthur_yoroii@dlsu.edu.ph>
2020-01-27 22:08:16 +0800

    initial commit

--- 11918403_exer1.py
+++ 11918403_exer1.py
@@ -6,6 +6,7 @@
 # Program Description
 """ The program allows you to solve one of 2 formulas which are Axial Deformation and Reynold's Number. 
 For Axial Deformation, you can solve the axial force, original length, cross-sectional area and the Modulus of Elasticity. 
-For the Reynold's number, you can solve the density, velocity, length of fluid travel, and fluid viscosity.
+For the Reynold's number, 
+you can solve the density, velocity, length of fluid travel, and fluid viscosity.
 """
 ##################################################
