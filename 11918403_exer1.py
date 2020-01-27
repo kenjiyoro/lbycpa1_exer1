70ceb27e48a1ff44501ee5c220dfdef195d92f6c
kyoro72901 <arthur_yoroii@dlsu.edu.ph>
2020-01-27 21:26:37 +0800

    initial commit

---
+++ 11918403_exer1.py
@@ -0,0 +1,11 @@
+##################################################
+# Author: Arthur Kenji T. Yoro II
+# Date: January 25, 2019
+# Course: LBYCPA1
+
+# Program Description
+""" The program allows you to solve one of 2 formulas which are Axial Deformation and Reynold's Number. 
+For Axial Deformation, you can solve the axial force, original length, cross-sectional area and the Modulus of Elasticity. 
+For the Reynold's number, you can solve the density, velocity, length of fluid travel, and fluid viscosity.
+"""
+##################################################
